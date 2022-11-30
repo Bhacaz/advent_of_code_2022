12583355b6f185c3cd3b62bcd67e10ca90e36c07
Jean-Francis Bastien <jfbastien@petalmd.com>
2022-11-30 13:16:52 -0500

    script add result as comment

--- main.rb
+++ main.rb
@@ -4,8 +4,10 @@ require 'byebug'
 day = ARGV[0].rjust(2, '0')
 part = ARGV[1]
 
+part_path = "day#{day}/part#{part}"
+
 require_relative 'base_part'
-require_relative "day#{day}/part#{part}"
+require_relative part_path
 
 result = nil
 allocated = 0
@@ -17,10 +19,23 @@ end
 
 duration = (measure.real * 1000).round(2)
 
-puts <<~result
+result_table = <<~result
 -------- Day: #{day}, Part: #{part} --------
 | Duration\t| #{duration} ms\t|
 | Allocated\t| #{allocated}\t\t|
 | Result\t| #{result}\t|
 ---------------------------------
 result
+
+puts result_table
+
+text = File.read(part_path + '.rb')
+result_comment = result_table.split("\n").map { |line| '# ' + line }.join("\n")
+
+if text[-4..].include?('end')
+  File.open(part_path + '.rb', 'w') { |file| file << text + "\n" + result_comment + "\n" }
+else
+  text_split = text.split("\n")
+  text_split.slice!(-5..)
+  File.open(part_path + '.rb', 'w') { |file| file << text_split.join("\n") + "\n" + result_comment + "\n" }
+end
